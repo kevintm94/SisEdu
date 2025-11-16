f41634aea553b7827a5f8bea60da7dc3f7afc05d
Kevin Trujillo <kevin.tm1994@outlook.com>
2025-11-16 07:27:59 -0400

    Agregar archivos de proyecto.

---
+++ SisEdu/Modelo/Docente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SisEdu.Modelo
+{
+    internal class Docente
+    {
+        public int id_docente { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string ci { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public int id_unidad { get; set; }
+
+        public Docente(int id_docente_, string nombre_, string apellido_, string ci_, string correo_, string telefono_, int id_unidad_)
+        {
+            this.id_docente = id_docente_;
+            this.nombre = nombre_;
+            this.apellido = apellido_;
+            this.ci = ci_;
+            this.correo = correo_;
+            this.telefono = telefono_;
+            this.id_unidad = id_unidad_;
+        }
+    }
+}
---
+++ SisEdu/Vista/LogIn.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SisEdu.Vista
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            txtUser = new TextBox();
+            txtPass = new TextBox();
+            btnIngresar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(82, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(82, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Contraseña";
+            // 
+            // txtUser
+            // 
+            txtUser.Location = new Point(164, 38);
+            txtUser.Name = "txtUser";
+            txtUser.Size = new Size(160, 23);
+            txtUser.TabIndex = 2;
+            // 
+            // txtPass
+            // 
+            txtPass.Location = new Point(164, 82);
+            txtPass.Name = "txtPass";
+            txtPass.PasswordChar = '*';
+            txtPass.Size = new Size(160, 23);
+            txtPass.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            btnIngresar.Location = new Point(144, 131);
+            btnIngresar.Name = "btnIngresar";
+            btnIngresar.Size = new Size(129, 25);
+            btnIngresar.TabIndex = 4;
+            btnIngresar.Text = "Ingresar";
+            btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // LogIn
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(432, 182);
+            Controls.Add(btnIngresar);
+            Controls.Add(txtPass);
+            Controls.Add(txtUser);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "LogIn";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LogIn";
+            Load += LogIn_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtUser;
+        private TextBox txtPass;
+        private Button btnIngresar;
+    }
+}
